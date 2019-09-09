f2db5fd53eaa5004f4342092d359ce9d18848a4b
GevorgArtenyan <53793770+GevorgArtenyan@users.noreply.github.com>
2019-09-09 19:11:27 +0400

    Tournament detail added

--- eSports/contest/urls.py
+++ eSports/contest/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('tournament/<slug:tournament_name_slug>/', views.tournament_detail, name='tournament-detail'),
 ]
--- eSports/contest/views.py
+++ eSports/contest/views.py
@@ -7,3 +7,16 @@ def home(request):
     context_dict['message'] = "All Tournaments"
     context_dict['tournaments'] = tournament_list
     return render(request, 'contest/home.html', context_dict)
+
+def tournament_detail(request, tournament_name_slug):
+    context_dict = {}
+    try:
+        tournament = Tournament.objects.get(slug=tournament_name_slug)
+        matches = Match.objects.filter(tournament=tournament)
+        context_dict['matches'] = matches
+        context_dict['tournament'] = tournament
+    except Tournament.DoesNotExist:
+        context_dict['tournament'] = None
+        context_dict['matches'] = None
+
+    return render(request, 'contest/tournament_detail.html', context_dict)
